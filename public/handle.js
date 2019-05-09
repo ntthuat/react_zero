1a3a476a46450125875eabe22455c9dbd38fa10d
ntthuat <nguyenthienthuat92@gmail.com>
2019-05-09 16:24:49 +0700

    _ Completed lesion 5

--- public/handle.js
+++ public/handle.js
@@ -5,3 +5,11 @@ ReactDOM.render(
   </div>,
   document.getElementById("root")
 );
+
+ReactDOM.render(
+  <div>
+    <h1 className="yellow">Thuat Nguyen</h1>
+    <h1>Thuat Nguyen</h1>
+  </div>,
+  document.getElementById("root2")
+);
