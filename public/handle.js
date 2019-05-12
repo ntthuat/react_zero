5ef32e54b41f1ff663b9cc77562a3697a58ca1ac
ntthuat <nguyenthienthuat92@gmail.com>
2019-05-12 20:59:13 +0700

    _ commit processing

--- public/handle.js
+++ public/handle.js
@@ -3,18 +3,33 @@ function getName(name) {
 }
 
 class ThuatNguyen extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      totalStudents: 10,
+      date: new Date()
+    };
+  }
+
   callGetname() {
     getName(this.props.teacher);
   }
+
   getInfo() {
     alert(this.props.children);
   }
+
+  addStudents() {
+    this.state.totalStudents++;
+  }
+
   render() {
     return (
       <div>
         <h1 className="yellow">
           Thuat Nguyen {this.props.name} - {this.props.teacher}{" "}
         </h1>
+        <div>Total of students: {this.state.totalStudents}</div>
         <p>{this.props.children}</p>
         <button onClick={this.getInfo.bind(this)}>Information</button>
         <button
@@ -25,6 +40,7 @@ class ThuatNguyen extends React.Component {
         >
           Information2
         </button>
+        <button onClick={this.addStudents.bind(this)}>Add students</button>
         <ThuatNguyen2 />
       </div>
     );
