96b1da06b91205305e8a565260a6e4e7e5e1bd68
ntthuat <nguyenthienthuat92@gmail.com>
2019-05-12 13:20:18 +0700

    _ Completed lesion 10

--- public/handle.js
+++ public/handle.js
@@ -1,12 +1,35 @@
 class ThuatNguyen extends React.Component {
+  getInfo() {
+    alert(this.props.children);
+  }
   render() {
-    return <h1 className="yellow"> Thuat Nguyen React </h1>;
+    return (
+      <div>
+        <h1 className="yellow">
+          Thuat Nguyen {this.props.name} - {this.props.teacher}{" "}
+        </h1>
+        <p>{this.props.children}</p>
+        <button onClick={this.getInfo.bind(this)}>Information</button>
+        <ThuatNguyen2 />
+      </div>
+    );
+  }
+}
+
+class ThuatNguyen2 extends React.Component {
+  render() {
+    return <h3> Lap Trinh ReactJs </h3>;
   }
 }
 
 ReactDOM.render(
   <div>
-    <ThuatNguyen />
+    <ThuatNguyen name="ReactJS" teacher="Mr.Khoa">
+      React Subject
+    </ThuatNguyen>
+    <ThuatNguyen name="NodeJS" teacher="Mr.Phu">
+      Node Subject
+    </ThuatNguyen>
   </div>,
   document.getElementById("root")
 );
