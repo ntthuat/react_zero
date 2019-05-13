89a837eaa4faf076f66cb53ce6194729fd6c6533
ntthuat <nguyenthienthuat92@gmail.com>
2019-05-13 16:55:01 +0700

    _ Completed lesion 17: example 3

---
+++ public/js/example3.js
@@ -0,0 +1,35 @@
+class Image extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      chosenImg: 1
+    };
+  }
+
+  changeImg() {
+    this.setState({
+      chosenImg: (this.state.chosenImg % 5) + 1
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <h2>Image</h2>
+        <img src={"images/" + this.state.chosenImg + ".png"} />
+      </div>
+    );
+  }
+
+  componentDidMount() {
+    setInterval(this.changeImg.bind(this), 1000);
+  }
+}
+
+ReactDOM.render(
+  <div>
+    <h1>Example 3</h1>
+    <Image />
+  </div>,
+  document.getElementById("exampleRoot3")
+);
