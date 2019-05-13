918d9ffffd52e3ed1bd9537e0659b096694c737d
ntthuat <nguyenthienthuat92@gmail.com>
2019-05-13 14:00:48 +0700

    _ Completed lesion 14

--- public/handle.js
+++ public/handle.js
@@ -54,8 +54,30 @@ class ThuatNguyen2 extends React.Component {
   }
 }
 
+class InputTag extends React.Component {
+  show() {
+    var text = this.refs.sl.value;
+    alert(text);
+  }
+
+  render() {
+    return (
+      <div>
+        <select ref="sl">
+          <option value="a">AAA</option>
+          <option value="b">BBB</option>
+          <option value="c">CCC</option>
+        </select>
+        <input type="text" ref="txt" />
+        <button onClick={this.show.bind(this)}>Show off</button>
+      </div>
+    );
+  }
+}
+
 ReactDOM.render(
   <div>
+    <InputTag />
     <ThuatNguyen name="ReactJS" teacher="Mr.Khoa" totalStudents="10">
       React Subject
     </ThuatNguyen>
