3d43f208a3bc2d1a5183c7075604bd6fc2cc9479
ntthuat <nguyenthienthuat92@gmail.com>
2019-05-15 15:05:32 +0700

    _ Completed lesion 21: example 6

--- public/js/example6.js
+++ public/js/example6.js
@@ -1,16 +1,49 @@
+var list;
+
 class Note6 extends React.Component {
   render() {
     return <div className="div-note6">{this.props.children}</div>;
   }
 }
 
+function addDiv() {
+  ReactDOM.render(<InputDiv />, document.getElementById("div-add"));
+}
+
 class List6 extends React.Component {
+  constructor(props) {
+    super(props);
+    list = this;
+    this.state = {
+      array: ["Hi", "Hello", "KhoaPham"]
+    };
+  }
+
   render() {
     return (
       <div className="div-list6">
-        <Note6>KhoaPham</Note6>
-        <Note6>Android</Note6>
-        <Note6>ReactJs</Note6>
+        <div id="div-add" />
+        <button onClick={addDiv}> Add</button>
+        {this.state.array.map(function(note, index) {
+          return <Note6 key={index}>{note}</Note6>;
+        })}
+      </div>
+    );
+  }
+}
+
+class InputDiv extends React.Component {
+  send() {
+    list.setState({
+      array: list.state.array.concat(this.refs.txt.value)
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <input type="text" ref="txt" placeholder="Enter your note" />
+        <button onClick={this.send.bind(this)}> Send</button>
       </div>
     );
   }
